f294861e061d9685937d7e5c8baf6ea70ec2dc84
Abda Rafi <abda_rafi@yahoo.com>
2023-01-14 09:20:50 +0000

    add multi select option for other cases

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import React, { useState } from "react";
 function App() {
   const [source, setSource] = useState("");
   const [result, setResult] = useState("");
+  const [caseOption, setCaseOption] = useState("kebab");
 
   function handleClick() {
-    // Replace spaces with hyphens
-    const hyphenated = source.replace(/\s+/g, "-");
-    // Convert the hyphenated string to kebab case
-    const kebab = hyphenated.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
-    // Set the result to the kebab case string
-    setResult(kebab);
+    let hyphenated = source.replace(/\s+/g, "-");
+
+    if (caseOption === "kebab") {
+      hyphenated = hyphenated.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+    } else if (caseOption === "camel") {
+      hyphenated = hyphenated.replace(/-([a-z])/g, (match) => match[1].toUpperCase());
+    } else if (caseOption === "snake") {
+      hyphenated = hyphenated.replace(/-/g, "_");
+    }
+    setResult(hyphenated);
   }
 
   return (
     <div>
       <textarea value={source} onChange={(e) => setSource(e.target.value)} />
       <textarea value={result} onChange={(e) => setResult(e.target.value)} />
+      <select onChange={(e) => setCaseOption(e.target.value)}>
+        <option value="kebab">Kebab Case</option>
+        <option value="camel">Camel Case</option>
+        <option value="snake">Snake Case</option>
+      </select>
       <button onClick={handleClick}>Convert</button>
     </div>
   );
 }
 
 export default App;
+

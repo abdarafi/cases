41c4423506517bbab93ece7fa7afa67e923a719f
Abda Rafi <abda_rafi@yahoo.com>
2023-01-29 01:15:34 +0000

    feat: auto to clipboard 2nd textarea

--- src/App.js
+++ src/App.js
@@ -12,23 +12,43 @@ function App() {
     if (caseOption === "kebab") {
       hyphenated = hyphenated.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
     } else if (caseOption === "camel") {
-      hyphenated = hyphenated.replace(/-([a-z])/g, (match) => match[1].toUpperCase());
+      hyphenated = hyphenated.replace(/-([a-z])/g, (match) =>
+        match[1].toUpperCase()
+      );
     } else if (caseOption === "snake") {
       hyphenated = hyphenated.replace(/-/g, "_");
     }
     setResult(hyphenated);
   }
 
+  function handleResultClick() {
+    navigator.clipboard.writeText(result);
+  }
+
   return (
     <div className="App">
-      <textarea className="input-box" value={source} onChange={(e) => setSource(e.target.value)} />
-      <textarea className="input-box" value={result} onChange={(e) => setResult(e.target.value)} />
-      <select className="select-box" onChange={(e) => setCaseOption(e.target.value)}>
+      <textarea
+        className="input-box"
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <textarea
+        className="input-box"
+        value={result}
+        onChange={(e) => setResult(e.target.value)}
+        onClick={handleResultClick}
+      />
+      <select
+        className="select-box"
+        onChange={(e) => setCaseOption(e.target.value)}
+      >
         <option value="kebab">Kebab Case</option>
         <option value="camel">Camel Case</option>
         <option value="snake">Snake Case</option>
       </select>
-      <button className="convert-button" onClick={handleClick}>Convert</button>
+      <button className="convert-button" onClick={handleClick}>
+        Convert
+      </button>
     </div>
   );
 }
